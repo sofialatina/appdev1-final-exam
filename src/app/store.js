abdc6c9574a96cb91b7a1c046075672ef6edbc63
Sofia Latina <sofiaisabellatina@gmail.com>
2025-11-29 13:11:14 +0800

    Implement redux store updated

--- src/app/store.js
+++ src/app/store.js
@@ -6,20 +6,3 @@ export const store = configureStore({
     todos: todosReducer,
   },
 });
-
-Update src/main.jsx to: 
-
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from "react-redux";
-import { store } from "./app/store";
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </StrictMode>,
-)
